4d24cbc1e4e8cd8eaa83c0e493251b3021532f39
dfhoughton <dfhougthon@gmail.com>
2024-10-11 10:36:56 -0400

    the theme

---
+++ util/theme.ts
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material"
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#721817",
+    },
+    secondary: {
+      main: "#fa9f42",
+    },
+  },
+  typography: {
+    fontFamily: "fantasy"
+  }
+})
